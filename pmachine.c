890ee1831b8b3cf9d8cd513ab09e50331fcf73fb
hsauers5@gmail.com <hsauers5@gmail.com>
2020-01-22 22:52:22 -0500

    constants

--- pmachine.c
+++ pmachine.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define STACK_HEIGHT 5000
+#define CODE_LENGTH 1000
+#define LEX_LEVEL 5
+
+int stack[STACK_HEIGHT];
+int lex[LEX_LEVEL];
+int code[CODE_LENGTH];
+
 int main(void) {
     printf("AAA");
 }
