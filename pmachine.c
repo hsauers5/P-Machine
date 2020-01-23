11b1c7246209f834e2f6d4e2101709646f965ce6
hsauers5@gmail.com <hsauers5@gmail.com>
2020-01-22 23:45:13 -0500

    Switch for arithmetic/logical operations

--- pmachine.c
+++ pmachine.c
@@ -10,8 +10,14 @@ int stack[MAX_DATA-STACK_HEIGHT];
 int lex[MAX_LEXI_LEVEL];
 int code[MAX_CODE_LENGTH];
 
+// registers
+int PC = 0;
+int IR = 0;
+int GP = -1;
+
 /* Begin Stack */
 /* ===========================================*/
+// these are also registers
 int BP = 0; // base pointer
 int SP = MAX_DATA-STACK_HEIGHT; // stack pointer
 
@@ -55,23 +61,16 @@ int stack_push(int data) {
         return 0;
     }
 }
-
 /* End Stack */
-/* ===========================================*/
 
 
 /* Begin P-Machine */
-
-int PC = 0;
-int IR = 0;
-int GP = -1;
-
 // instruction format from file is {OP LEVEL M} space-separated.
 typedef struct {
    int op;
    int l;
    int m;
-}instruction;
+} instruction;
 
 /* find base L levels down */
 // l stand for L in the instruction format
@@ -86,9 +85,58 @@ int base(int l, int base) {
 }
 
 
-int do_operation(instruction * inst) {
-    switch(inst->op) {
-        
+int do_operation(int operation) {
+    switch(operation) {
+        case 01:
+            // LIT, 0, M
+            // Push literal M onto data or stack
+            break;
+        case 02:
+            // OPR, 0, M
+            // Operation to be performed on the data at the top of the stack
+            break;
+        case 03:
+            // LOD, L, M
+            // Load value to top of stack from the stack location at offset M from L lexicographical levels down
+            // ??
+            break;
+        case 04: 
+            // STO, L, M
+            // Store value at top of stack in the stack location at offset M from L lexi levels down
+            break;
+        case 05:
+            // CAL, L, M
+            // Call procedure at code index M
+            // Generate new activation record and pc <- M
+            break;
+        case 06:
+            // INC 0, M
+            // Allocate M locals, increment SP by M. 
+            // First 3 are Static Link (SL), Dynamic Link (DL), and return address (RA)
+            break;
+        case 07:
+            // JMP 0, M
+            // Jump to instruction M
+            break;
+        case 8:
+            // JPC 0, M
+            // Jump to instruction M if top stack element == 0
+            break;
+        case 9:
+            // SIO 0, 1
+            // Write top of stack to screen
+            // pop? peek? who knows
+            break;
+        case 10:
+            // SIO 0, 2
+            // Read in input from user and store at top of stack
+            // Input safety? Format? 
+            break;
+        case 11: 
+            // SIO 0, 3
+            // End of program. 
+            exit(0);
+            break;
     }
 }
 
