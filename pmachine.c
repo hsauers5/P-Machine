667b44156a2a224db1f3f6fe8a753a2c7b65d157
alexogilbee <alexogilbee@github.com>
2020-01-26 20:24:16 -0500

    Doesn't seg fault, so yay i guess

--- pmachine.c
+++ pmachine.c
@@ -14,14 +14,14 @@ int lex[MAX_LEXI_LEVEL];
 int code[MAX_CODE_LENGTH];	// pretty sure this needs to be an instruction pointer array		|| obsolete as well.
 
 // output mega strings (might go 2-d later on)
-char * output_one = {"Line\tOP\tR\tL\tM\n"};
+char ** output_one;
 char * output_two = {"\t\tpc\tbp\tsp\tregisters\nInitial values\t"};
 
-void update_output_one(char * OP, int R, int L, int M);
+void update_output_one(char * OP, int R, int L, int M, int i);
 void update_output_two();
 char * dynamic_strcat(char * base, char * added);
 // strangely: if halt is false then program should halt, else continue
-int halt = -1;
+int halt = 0;
 
 // registers
 int PC = 0;
@@ -121,18 +121,12 @@ int MATH(int OP, int R, int L, int M) {
             // NEG
             // stack[SP] = -stack[SP];
             	REG[R] = -REG[L];
-				update_output_one("neg", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 13:
             // ADD
             /* SP += 1;
             stack[SP] = stack[SP] + stack[SP - 1]; */
             	REG[R] = REG[L] + REG[M];
-				update_output_one("add", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 14: 
             // SUB
@@ -141,9 +135,6 @@ int MATH(int OP, int R, int L, int M) {
             stack[SP] = stack[SP] - stack[SP - 1];
             */
             	REG[R] = REG[L] - REG[M];
-				update_output_one("sub", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 15:
             // MUL
@@ -152,9 +143,6 @@ int MATH(int OP, int R, int L, int M) {
             stack[SP] = stack[SP] * stack[SP - 1];
             */
             	REG[R] = REG[L] * REG[M];
-				update_output_one("mul", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 16:
             // DIV
@@ -163,9 +151,6 @@ int MATH(int OP, int R, int L, int M) {
             stack[SP] = stack[SP] / stack[SP - 1];
             */
             	REG[R] = REG[L] / REG[M];
-				update_output_one("div", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 17:
             // ODD
@@ -173,9 +158,6 @@ int MATH(int OP, int R, int L, int M) {
             // stack[sp] = stack[sp] mod 2) or ord(odd(stack[sp]))
           	// stack[SP] = stack[SP] % 2;
             	REG[R] = REG[R] % 2;
-				update_output_one("odd", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 18:
             // MOD
@@ -184,9 +166,6 @@ int MATH(int OP, int R, int L, int M) {
             stack[SP] = stack[SP] % stack[SP - 1];
             */
             	REG[R] = REG[L] % REG[M];
-				update_output_one("mod", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 19:
             // EQL
@@ -195,9 +174,6 @@ int MATH(int OP, int R, int L, int M) {
             stack[SP] = stack[SP] == stack[SP - 1];
             */
             	REG[R] = REG[L] == REG[M];
-				update_output_one("eql", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 20:
             // NEQ
@@ -206,9 +182,6 @@ int MATH(int OP, int R, int L, int M) {
             stack[SP] = stack[SP] != stack[SP - 1];
             */
             	REG[R] = REG[L] != REG[M];
-				update_output_one("neq", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 21:
             // LSS
@@ -217,9 +190,6 @@ int MATH(int OP, int R, int L, int M) {
             stack[SP] = stack[SP] < stack[SP - 1];
             */
             	REG[R] = REG[L] < REG[M];
-				update_output_one("lss", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 22:
             // LEQ
@@ -228,9 +198,6 @@ int MATH(int OP, int R, int L, int M) {
             stack[SP] = stack[SP] <= stack[SP - 1];
             */
             	REG[R] = REG[L] <= REG[M];
-				update_output_one("leq", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 23:
             // GTR
@@ -239,9 +206,6 @@ int MATH(int OP, int R, int L, int M) {
             stack[SP] = stack[SP] > stack[SP - 1];
             */
             	REG[R] = REG[L] > REG[M];
-				update_output_one("gtr", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 24:
             // GEQ
@@ -250,9 +214,6 @@ int MATH(int OP, int R, int L, int M) {
             stack[SP] = stack[SP] >= stack[SP - 1];
             */
             	REG[R] = REG[L] >= REG[M];
-				update_output_one("geq", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
     }
 }
@@ -353,97 +314,67 @@ int do_operation(instruction instr) {
     int L = instr.l;
     int operation = instr.op;
     int R = instr.reg;
+
+	PC++;
+
     switch(operation) {
         case 01:
             // LIT, R, 0, M
             // Push literal M onto data or stack
             	LIT(R, 0, M);
-				update_output_one("lit", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 02:
             // RTN, 0, 0, 0
             // Operation to be performed on the data at the top of the stack
             	RTN(0, 0, 0);
-				update_output_one("rtn", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 03:
             // LOD, R, L, M
             // Load value to top of stack from the stack location at offset M from L lexicographical levels down
             	LOD(R, L, M);
-				update_output_one("lod", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 04: 
             // STO, R, L, M
             // Store value at top of stack in the stack location at offset M from L lexi levels down
             	STO(R, L, M);
-				update_output_one("sto", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 05:
             // CAL, 0, L, M
             // Call procedure at code index M
             // Generate new activation record and pc <- M
             	CAL(0, L, M);
-				update_output_one("cal", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 06:
             // INC 0, 0, M
             // Allocate M locals, increment SP by M. 
             // First 3 are Static Link (SL), Dynamic Link (DL), and return address (RA)
             	INC(0, 0, M);
-				update_output_one("inc", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 07:
             // JMP 0, 0, M
             // Jump to instruction M
             	JMP(0, 0, M);
-				update_output_one("jmp", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 8:
             // JPC, R, 0, M
             // Jump to instruction M if top stack element == 0
             	JPC(R, 0, M);
-				update_output_one("jpc", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 9:
             // SIO, R, 0, 1
             // Write top of stack to screen
             // pop? peek? who knows
             	SIO(R, 0, 1);
-				update_output_one("sio", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 10:
             // SIO, R, 0, 2
             // Read in input from user and store at top of stack
             	SIO(R, 0, 2);
-				update_output_one("sio", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         case 11: 
             // SIO, R, 0, 3
             // End of program: halt condition
             	SIO(R, 0, 3);
-				update_output_one("sio", R, L, M);
-				output_two = dynamic_strcat(output_two, "\t\t");
-				update_output_two();
             break;
         default:
             if (operation >= 12) {
@@ -451,6 +382,9 @@ int do_operation(instruction instr) {
             }
             break;
     }
+		printf("fahjfhl;asfd\n");
+		output_two = dynamic_strcat(output_two, output_one[PC-1]);
+		update_output_two();
 }
 
 // write a control method and handle input
@@ -479,21 +413,19 @@ char * dynamic_strcat(char * base, char * added) {
 	return strcat(conjoined, added);
 }
 
-void update_output_one(char * OP, int R, int L, int M) {
+void update_output_one(char * OP, int R, int L, int M, int i) {
 	char tmp[3];
-
-	output_one = dynamic_strcat(output_one, OP);
-	
+	printf("gamer word %d\n", i);
+	output_one[i] = dynamic_strcat(output_one[i], OP);
+	printf("ni-\n");
 	sprintf(tmp, "\t%d", R);
-	output_one = dynamic_strcat(output_one, tmp);
+	output_one[i] = dynamic_strcat(output_one[i], tmp);
+	printf("sad days\n");
+	sprintf(tmp, " %d", L);
+	output_one[i] = dynamic_strcat(output_one[i], tmp);
 
-	sprintf(tmp, "\t%d", L);
-	output_one = dynamic_strcat(output_one, tmp);
-
-	sprintf(tmp, "\t%d", M);
-	output_one = dynamic_strcat(output_one, tmp);
-
-	output_one = dynamic_strcat(output_one, "\n");
+	sprintf(tmp, " %d", M);
+	output_one[i] = dynamic_strcat(output_one[i], tmp);
 
 	return;
 }
@@ -518,15 +450,98 @@ void update_output_two() {
 	}
 	sprintf(tmp, "%d\n", REG[REG_FILE_LENGTH - 1]);
 	output_two = dynamic_strcat(output_two, tmp);
+
 	// update the stack
 	output_two = dynamic_strcat(output_two, "Stack:");
-	for (i = 0; i < MAX_DATA_STACK_HEIGHT; i++) {
+	for (i = 0; i < SP; i++) {
 		sprintf(tmp, " %d", stack[i]);
 		output_two = dynamic_strcat(output_two, tmp);		
 	}
 	output_two = dynamic_strcat(output_two, "\n\n");		
 
 	return;
+}
+
+char * ins_to_string(instruction ins, int i) {
+	char * tmp = (char *)calloc(4, sizeof(char));
+
+	// determine the OP
+	switch(ins.op) {
+		case 1:
+			tmp = "lit";
+			break;
+		case 2:
+			tmp = "rtn";
+			break;
+		case 3:
+			tmp = "lod";
+			break;
+		case 4:
+			tmp = "sto";
+			break;
+		case 5:
+			tmp = "cal";
+			break;
+		case 6:
+			tmp = "inc";
+			break;
+		case 7:
+			tmp = "jmp";
+			break;
+		case 8:
+			tmp = "jpc";
+			break;
+		case 9:
+		case 10:
+		case 11:
+			tmp = "sio";
+			break;
+		case 12:
+			tmp = "neg";
+			break;
+		case 13:
+			tmp = "add";
+			break;
+		case 14:
+			tmp = "sub";
+			break;
+		case 15:
+			tmp = "mul";
+			break;
+		case 16:
+			tmp = "div";
+			break;
+		case 17:
+			tmp = "odd";
+			break;
+		case 18:
+			tmp = "mod";
+			break;
+		case 19:
+			tmp = "eql";
+			break;
+		case 20:
+			tmp = "neq";
+			break;
+		case 21:
+			tmp = "lss";
+			break;
+		case 22:
+			tmp = "leq";
+			break;
+		case 23:
+			tmp = "gtr";
+			break;
+		case 24:
+			tmp = "geq";
+			break;
+		default:
+			printf("literally not possible");
+}
+	// function the rest
+		update_output_one(tmp, ins.reg, ins.l, ins.m, i);
+
+	return output_one[i];
 }
 
 int main(void) {
@@ -554,21 +569,47 @@ int main(void) {
 	// record number of operations while reading from input.txt
 	lines_of_text = read_in(fp, text);
 
-	// use said text array for the mega-while loop
-	update_output_two();
+	// initialize output one double array
+	output_one = (char **)calloc(lines_of_text, sizeof(char *));
+	// loop to create output one
 	for (i = 0; i < lines_of_text; i++) {
+		printf("%d\n", i);
+		printf("yeet\n");
+		output_one[i] = (char *)calloc(1, sizeof(char));
+		printf("yah\n");
 		sprintf(line_index, "%d\t", i);
-		output_one = dynamic_strcat(output_one, line_index);
-		do_operation(text[i]);
+		printf("yote\n");
+		output_one[i] = dynamic_strcat(output_one[i], line_index);
+		printf("schleet\n");
+		output_one[i] = ins_to_string(text[i], i);
 	}
 
+	// use said text array for the mega-while loop
+	update_output_two();
+	while(!halt) {
+		do_operation(text[PC]);
+	}
+
+	printf("pass the boof\n");
+
+	for (i = 0; i < lines_of_text; i++) {
+		printf("%s\n", output_one[i]);
+	}
+	printf("%s\n", output_two);
 	// write the strings to the output file		
 	fclose(fp);
+	printf("close the sesame\n");
 	fp = fopen("output.txt", "w");
-	fprintf(fp, "%s\n%s", output_one, output_two);
+	
+	fprintf(fp, "Line\tOP\tR L M\n");
+	for (i = 0; i < lines_of_text; i++) {
+		fprintf(fp, "%s\n", output_one[i]);
+	}
+	fprintf(fp, "%s\n", output_two);
 
 	fclose(fp);
 	free(text);
+	free(output_one);
 	return 0;
 }
 
